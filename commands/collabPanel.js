a3163d9f84767833178adb6dd6042bdacb49bb83
shbase00 <sh.base00@gmail.com>
2026-03-09 15:26:02 +0200

    Update collabPanel.js

--- commands/collabPanel.js
+++ commands/collabPanel.js
@@ -1,35 +1,35 @@
 const {
-SlashCommandBuilder,
-PermissionFlagsBits,
-ActionRowBuilder,
-ButtonBuilder,
-ButtonStyle,
-StringSelectMenuBuilder
+  SlashCommandBuilder,
+  PermissionFlagsBits,
+  ActionRowBuilder,
+  StringSelectMenuBuilder
 } = require('discord.js');
-
-const db = require('../db');
-
-const PAGE_SIZE = 10;
 
 module.exports = {
 
 data: new SlashCommandBuilder()
 .setName('collab_panel')
-.setDescription('View collabs')
+.setDescription('View collabs panel')
 .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
 
 async execute(interaction) {
 
 await interaction.reply({
-content:"Choose which collabs you want to view:",
+content:"Select which collabs to view:",
 components:[
 new ActionRowBuilder().addComponents(
 new StringSelectMenuBuilder()
-.setCustomId("collab_type")
-.setPlaceholder("Select collab type")
+.setCustomId("collab_filter")
+.setPlaceholder("Choose collab type")
 .addOptions([
-{label:"Active Collabs",value:"active"},
-{label:"Closed Collabs",value:"closed"}
+{
+label:"Active Collabs",
+value:"active"
+},
+{
+label:"Closed Collabs",
+value:"closed"
+}
 ])
 )
 ],
