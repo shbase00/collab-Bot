7d2f3ad2bc8a0d537f1541bc5d1069a217b0d370
shbase00 <sh.base00@gmail.com>
2026-03-09 15:30:59 +0200

    Create export_data.js

---
+++ commands/export_data.js
@@ -0,0 +1,46 @@
+const {
+  SlashCommandBuilder,
+  PermissionFlagsBits
+} = require('discord.js');
+
+const fs = require("fs");
+const db = require("../db");
+
+module.exports = {
+
+data: new SlashCommandBuilder()
+.setName("export_data")
+.setDescription("Export all database data")
+.setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+
+async execute(interaction){
+
+await interaction.deferReply({ephemeral:true});
+
+const collabs = db.prepare("SELECT * FROM collabs").all();
+const submissions = db.prepare("SELECT * FROM submissions").all();
+
+let csv = "TYPE,ID,NAME,STATUS,USER,TIER,COMMUNITY,CONTEST,WALLET\n";
+
+for(const c of collabs){
+
+csv += COLLAB,${c.id},${c.name},${c.status},,,,,\n;
+
+}
+
+for(const s of submissions){
+
+csv += SUBMISSION,${s.id},,${s.collab_id},${s.username},${s.tier},${s.community},${s.contest_link},${s.sheet_link}\n;
+
+}
+
+fs.writeFileSync("export.csv",csv);
+
+await interaction.editReply({
+content:"Database exported",
+files:["export.csv"]
+});
+
+}
+
+};
